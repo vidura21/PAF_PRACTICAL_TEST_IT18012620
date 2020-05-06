16cc6e019e1869ca8525731bed2233546db63d79
vidura21 <vidurasamarasiri@gmail.com>
2020-05-06 15:49:11 +0530

    added the payment function

---
+++ Payment_Service/src/model/PaymentsAPI.java
@@ -0,0 +1,99 @@
+package model;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+/**
+ * Servlet implementation class PaymentsApi
+ */
+@WebServlet("/PaymentsAPI")
+public class PaymentsAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Payment payObj = new Payment();
+
+	public PaymentsAPI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String output = payObj.insertPayment(request.getParameter("firstname"), request.getParameter("lastname"),
+				request.getParameter("ammount"), request.getParameter("cardnumber"), request.getParameter("date"),
+				request.getParameter("cvv"));
+		response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+
+	private static Map getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			String[] params = queryString.split("&");
+			for (String param : params) {
+
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+		} catch (Exception e) {
+		}
+		return map;
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		{
+			Map paras = getParasMap(request);
+			String output = payObj.updatePayment(paras.get("hidItemIDSave").toString(),
+					paras.get("firstname").toString(), paras.get("lastname").toString(),
+					paras.get("ammount").toString(), paras.get("cardnumber").toString(), paras.get("date").toString(),
+					paras.get("cvv").toString());
+			response.getWriter().write(output);
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Map paras = getParasMap(request);
+		String output = payObj.deletePayment(paras.get("paymentID").toString());
+		response.getWriter().write(output);
+	}
+
+}
